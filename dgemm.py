513b56f78962222d8986ba44508d9e87a3eab866
xiaoyu12 <xiaoyu.zhang@gmail.com>
2021-08-25 09:25:02 -0700

    Initial commit of matrix multiplication code from computer organization and design

---
+++ dgemm.py
@@ -0,0 +1,27 @@
+# This is a sample Python script for matrix multiplication.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import numpy as np
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    n = 960
+    #A = np.random.rand(n, n)
+    #B = np.random.rand(n, n)
+    #C = np.zeros((n, n))
+
+    A = [[ 1.0 for i in range(n) ] for j in range(n) ]
+    B = [[ 1.0 for i in range(n) ] for j in range(n) ]
+    C = [[ 0.0 for i in range(n) ] for j in range(n) ]
+
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                C[i][j] += A[i][k] * B[k][j]
+    
+
+    print(C[0][0])
+
---
+++ dgemm_ch6.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <x86intrin.h>
+#define UNROLL (4)
+#define BLOCKSIZE 32
+void do_block (int n, int si, int sj, int sk,
+               double *A, double *B, double *C)
+{ 
+   for ( int i = si; i < si + BLOCKSIZE; i += UNROLL * 8 )
+      for ( int j = sj; j < sj + BLOCKSIZE; j++ ) {
+         __m512d c[UNROLL];
+         for ( int r = 0; r < UNROLL; r++ )
+            c[r] = _mm512_load_pd(C + i + r * 8 + j * n); //[ UNROLL];
+        
+         for( int k = sk; k < sk + BLOCKSIZE; k++ )
+         {
+            __m512d bb = _mm512_broadcastsd_pd(_mm_load_sd(B + j * n + k));
+            for (int r = 0; r < UNROLL; r++)
+               c[r] = _mm512_fmadd_pd(_mm512_load_pd(A + n * k + r * 8 + i), bb, c[r]);
+         }
+
+         for ( int r = 0; r < UNROLL; r++ )
+            _mm512_store_pd(C + i + r * 8 + j * n, c[r]);
+      }
+}
+
+
+void dgemm(int n, double* A, double* B, double* C)
+{
+#pragma omp parallel for
+for ( int sj = 0; sj < n; sj += BLOCKSIZE )
+     for ( int si = 0; si < n; si += BLOCKSIZE )
+          for ( int sk = 0; sk < n; sk += BLOCKSIZE )
+             do_block(n, si, sj, sk, A, B, C);
+}
+
+int main(int argc, char* argv[]) {
+    int n = 960;
+    double *A = aligned_alloc(64, sizeof(double) * n * n);
+    double *B = aligned_alloc(64, sizeof(double) * n * n);
+    double *C = aligned_alloc(64, sizeof(double) * n * n);
+
+     for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            A[i + j*n] = 1.0;
+            B[i + j*n] = 1.0;
+            C[i + j*n] = 0.0;
+        }
+    }
+    /* call the matrix multiplication function */
+    dgemm(n, A, B, C);
+
+    printf("%f\n", C[0]);
+}
